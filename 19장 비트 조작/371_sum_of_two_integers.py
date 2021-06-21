d4b3a9b1cf8904e7080716f884639aa520b97dd5
rmsqor103 <rmsqor103@naver.com>
2021-06-21 16:43:00 +0900

    day 37

---
+++ 19장 비트 조작/371_sum_of_two_integers.py
@@ -0,0 +1,52 @@
+class Solution:
+    # solve 1
+    def getSum(self, a: int, b: int) -> int:
+        MASK = 0xFFFFFFFF
+        INT_MAX = 0x7FFFFFFF
+
+        a_bin = bin(a & MASK)[2:].zfill(32)
+        b_bin = bin(b & MASK)[2:].zfill(32)
+
+        result = []
+        carry = 0
+        sum = 0
+        for i in range(32):
+            A = int(a_bin[31-i])
+            B = int(b_bin[31-i])
+
+            Q1 = A & B
+            Q2 = A ^ B
+            Q3 = Q2 & carry
+            sum = carry ^ Q2
+            carry = Q1 | Q3
+
+            result.append(str(sum))
+        if carry == 1:
+            result.append('1')
+
+        result = int(''.join(result[::-1]), 2) & MASK
+
+        if result > INT_MAX:
+            result = ~(result ^ MASK)
+
+        return result
+
+    # solve 2
+    def getSum_2(self, a: int, b: int) -> int:
+        MASK = 0xFFFFFFFF
+        INT_MAX = 0x7FFFFFFF
+
+        while b != 0:
+            a, b = (a ^ b) & MASK, ((a & b) << 1) & MASK
+        if a > INT_MAX:
+            a = ~(a ^ MASK)
+        return a
+
+
+sol = Solution()
+print(sol.getSum(1,2))
+
+"""
+Input: 1,2
+Output: 3
+"""
